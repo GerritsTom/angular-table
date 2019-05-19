166394181395016201bd210d036eae88f95dddb3
tomgerrits <tomgerrits@gmx.de>
2019-05-19 20:01:32 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
+
+  title = 'angular-table';
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers'
+    };
+  }
+}
